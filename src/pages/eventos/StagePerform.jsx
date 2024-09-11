45e0f500c3e6cd512f67c085e3df58575806c37e
gleiva <gleiva@unca.edu.ar>
2024-09-11 12:10:33 -0300

    falta eliminar invitados de la lista y generar la lista de IDs

--- src/pages/eventos/StagePerform.jsx
+++ src/pages/eventos/StagePerform.jsx
@@ -1,7 +1,7 @@
 import Card from '@/components/ui/Card'
 import React, { useState } from 'react'
 import Loading from '@/components/Loading'
-import columnContact from '@/json/columnsContact.json'
+// import columnContact from '@/json/columnsContact.json'
 
 import { set, useForm } from 'react-hook-form'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -11,12 +11,22 @@ import { toast } from 'react-toastify'
 import { useQuery } from '@tanstack/react-query'
 import { getContacts } from '@/services/contactService'
 
-
+const columnContact = [{
+    "label": "Apellido y Nombre",
+    "field": "apellido"
+}, {
+    "label": "Correo Electronico",
+    "field": "email"
+}, {
+    "label": "Telefono",
+    "field": "telefono"
+}]
 export const StagePerform = () => {
 
     const navigate = useNavigate()
     const [search, setSearch] = useState('')
     const [contacts, setContact] = useState([])
+    const [invitados, setInvitados] = useState([])
     // const [, setFormData] = useState(initialForm)
     const { id } = useParams()
     const [currentPage, setCurrentPage] = useState(1)
@@ -29,23 +39,41 @@ export const StagePerform = () => {
         return <Loading />
     }
 
-    const onSearch = () => {
-        console.log(search)
-    }
+
     const handleChange = async (e) => {
         setSearch(e.target.value)
-
         if (search.length > 2) {
-
             const res = await searchContact(search)
-
             setContact(res.items)
-            console.log(contacts);
         }
         else {
             setContact([])
         }
     }
+
+    const addContact = (e) => {
+
+        if (invitados.length === 0) {
+            setInvitados([...invitados, e])
+            setContact([])
+        }
+        else {
+            const exists = invitados.some((invitado) => invitado.id === e.id);
+            if (!exists) {
+                // Si no existe, agregar el nuevo invitado
+                setInvitados([...invitados, e]);
+                setContact([])
+            } else {
+                console.log("El invitado ya existe");
+            }
+        }
+    }
+
+    const deleteGuest=(e)=>{
+        console.log(e);
+        const exists = invitados.findIndex((invitado) => invitado.id === e);
+    }
+
     return (
 
         <>
@@ -57,85 +85,97 @@ export const StagePerform = () => {
                                 <h1 className='text-2xl font-semibold dark:text-white mb-4 md:mb-0'>Pasar evento a Realizar</h1>
                             </div>
                         </Card>
-
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
-                            </div>
-                            <input type="search" onChange={handleChange} id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ingrese el nombre del invitado" value={search} />
-                            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                        </div>
-
-                        {<div class="w-1/2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                            {contacts.map((contact) => {
-                                return (
-                                    <button
-                                        aria-current="true"
-                                        key={contact.id}
-                                        type="button"
-                                        className="w-full px-4 py-2 font-medium text-left rtl:text-right text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer focus:outline-none dark:bg-gray-800 dark:border-gray-600"
-                                    >
-                                        {contact.nombre} {contact.apellido} | {contact.email}
-                                    </button>
-                                );
-                            })}
-
-
-
-                        </div>}
+                        <Card>
+                            <div className="w-full flex gap-5">
+                                <div className='w-1/2'>
+                                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                    <div className="relative">
+                                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            </svg>
+                                        </div>
+                                        <input type="search" onChange={handleChange} id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ingrese el nombre del invitado" value={search} />
+                                        {/* <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
+                                    </div>
+                                    {<div className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                        {contacts.map((contact) => {
+                                            return (
+                                                <button
+                                                    onClick={() => addContact(contact)}
+                                                    aria-current="true"
+                                                    key={contact.id}
+                                                    type="button"
+                                                    className="w-full px-4 py-2 font-medium text-left rtl:text-right text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer focus:outline-none dark:bg-gray-800 dark:border-gray-600"
+                                                >
+                                                    {contact.nombre} {contact.apellido} | {contact.email}
+                                                </button>
+                                            );
+                                        })}
 
 
-                        {/* <Card noborder>
-                            <div className='w-1/2'>
-                                <p className='uppercase'>Selecione los invitados</p>
-                                <div className='overflow-x-auto -mx-6'>
-                                    <div className='inline-block min-w-full align-middle'>
-                                        <div className='overflow-hidden '>
-                                            <table className='min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700'>
-                                                <thead className='bg-slate-200 dark:bg-slate-700'>
-                                                    <tr>
-                                                        {columnContact.map((column, i) => (
-                                                            <th key={i} scope='col' className='table-th'>
-                                                                {column.label}
-                                                            </th>
-                                                        ))}
-                                                    </tr>
-                                                </thead>
-                                                <tbody className='bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700'>
-                                                    {
-                                                        (contactos.items.length > 0)
-                                                            ? (contactos.items.map((contacto) => {
-                                                                return (
-                                                                    <tr key={contacto.id}>
-                                                                        <td className='table-td'>{contacto.apellido} {contacto.nombre}</td>
 
-                                                                        <td className='table-td'>{contacto.email}</td>
-                                                                        <td className='table-td'>{contacto.telefono}</td>
-                                                                        <td className='table-td'>
-                                                                        <input type="checkbox"/> Agregar
-                                                                        </td>
+                                    </div>}
+                                </div>
 
-                                                                    </tr>
-                                                                )
-                                                            }))
-                                                            : (<tr><td colSpan='10' className='text-center py-2 dark:bg-gray-800'>No se encontraron resultados</td></tr>)
-                                                    }
-                                                </tbody>
 
-                                            </table>
 
-                                       
 
+                                <div className='w-1/2'>
+
+                                    <div className='overflow-x-auto mx-6'>
+                                        <div className='inline-block min-w-full align-middle'>
+                                            <div className='overflow-hidden '>
+                                                <table className='min-w-full divide-y divide-slate-100 table-fixed dark:divide-slate-700'>
+                                                    <thead className='bg-slate-200 dark:bg-slate-700'>
+                                                        <tr>
+                                                            {columnContact.map((column, i) => (
+                                                                <th key={i} scope='col' className='table-th'>
+                                                                    {column.label}
+                                                                </th>
+                                                            ))}
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody className='bg-white divide-y divide-slate-100 dark:bg-slate-800 dark:divide-slate-700'>
+                                                        {
+                                                            (invitados.length > 0)
+                                                                ? (invitados.map((contacto) => {
+                                                                    return (
+                                                                        <tr key={contacto.id}>
+                                                                            <td className='table-td'>{contacto.apellido} {contacto.nombre}</td>
+                                                                            <td className='table-td'>{contacto.email}</td>
+                                                                            <td className='table-td'>{contacto.telefono}</td>
+                                                                            <td className='table-td flex justify-start gap-2'></td>
+                                                                            <td onClick={()=>{deleteGuest(contacto.id)}}>X</td>
+                                                                        </tr>
+                                                                    )
+                                                                }))
+                                                                : (<tr><td colSpan='10' className='text-center py-2 dark:bg-gray-800'>No se encontraron resultados</td></tr>)
+                                                        }
+                                                    </tbody>
+
+                                                </table>
+
+                                                {/* Paginado */}
+                                                {/* <div className='flex justify-center mt-8'>
+                                            <Pagination
+                                                paginate={{
+                                                    current: contactos.current,
+                                                    totalPages: contactos.totalPages
+                                                }}
+                                                onPageChange={(page) => setCurrentPage(page)}
+                                                text
+                                            />
+                                        </div> */}
+
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
 
+                        </Card>
 
-                        </Card> */}
 
 
                         <div className='flex justify-end gap-4 mt-8'>
