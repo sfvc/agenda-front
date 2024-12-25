cb39ee70aab1417d69bd37445865a42f538b7c0b
Nahue <nahuelsoriap@gmail.com>
2024-12-25 12:40:40 -0300

    .

--- src/components/agenda/forms/SelectForm.jsx
+++ src/components/agenda/forms/SelectForm.jsx
@@ -14,12 +14,12 @@ export const SelectForm = ({ register, title, options = [], error = null, disabl
         disabled={disabled}
         onChange={onChange}
       >
-        <option value='' >Seleccione una opción</option>
+        <option value=''>Seleccione una opción</option>
         {options.map(op => (
           <option key={op.id} value={op.id}>{op.nombre}</option>
         ))}
       </select>
       {error && <p className='mt-2 text-danger-500 block text-sm'>{error.message}</p>}
     </div>
-  );
+  )
 }
