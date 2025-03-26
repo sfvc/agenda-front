41aabf25a1fb12b7bd531c5dca4a6ff0fcc2c8ef
Nahuewe <nahuelsoriap@gmail.com>
2025-03-26 18:15:39 -0300

    Añadidos todos los graficos al Dashboard

--- src/api/graficosApi.js
+++ src/api/graficosApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const graficosApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
+  // baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
+  baseURL: 'http://137.184.4.18:7050/api'
 })
 
 graficosApi.interceptors.request.use(config => {
