7d28e8a1987c0511b4fb00c53d402945a63080d0
Nahue <nahuelsoriap@gmail.com>
2025-03-27 19:23:58 -0300

    Agregada la nueva api para los graficos

--- src/api/graficosApi.js
+++ src/api/graficosApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const graficosApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
+  // baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
+  baseURL: 'https://api.datos-agenda.cc.gob.ar/api'
 })
 
 graficosApi.interceptors.request.use(config => {
