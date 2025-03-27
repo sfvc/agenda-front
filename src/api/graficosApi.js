8f9261f60446999a71f274cf9ef4cfd56a22cdd5
Nahue <nahuelsoriap@gmail.com>
2025-03-27 19:06:37 -0300

    Agregada la nueva api para los graficos

--- src/api/graficosApi.js
+++ src/api/graficosApi.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const graficosApi = axios.create({
-  // baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
-  baseURL: 'http://137.184.4.18:7050/api'
+  baseURL: `${import.meta.env.VITE_API_URL_GRAFICS}`
 })
 
 graficosApi.interceptors.request.use(config => {
